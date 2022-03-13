693899b038f5f3a7281ac15dcea989865f420191
Submits <101422599+Submits@users.noreply.github.com>
2022-03-13 04:06:48 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+var faunadb = window.faunadb
+var q = faunadb.query
+var client = new faunadb.Client({
+  secret: 'fnAEhgo6CoAAwWq8oF1VfjvulHEylVcOMB2UsUj9',
+  domain: 'db.eu.fauna.com',
+  scheme: 'https',
+})
+
+function sleep(milisec) {
+    return new Promise(resolve => {
+    setTimeout(() => { resolve('') }, milisec);
+    })
+    }
+
+let users = []
+
+async function getAllUsers(){
+
+    client
+    .query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection("users")), { size: 1000 }),
+        q.Lambda("X", q.Get(q.Var("X")))
+      )
+    )
+    .then(function (x) {
+        for (let i = 0; i < x.data.length; i++) {
+        users.push({id: x.data[i].data.id, username:  x.data[i].data.username, profile_image:  x.data[i].data.profile_image, user_colour: x.data[i].data.colour})
+        }
+
+    });
+
+    console.log(users)
+   
+
+}
+
+async function getPasteInfo(){
+
+      getAllUsers()
+      await sleep(500)
+      getPaste()
+  }
+
+  function getPaste(){
+
+    
+    let urlParams = new URLSearchParams(window.location.search);
+
+    client.query(
+        q.Get(
+          q.Match(q.Index('paste_by_id'), urlParams.get('id'))
+        )
+      )
+      .then(function(ret) {    
+        let content = ret.data.content 
+        let userinfo = users.findIndex(function(item, i) {
+            return item.id === ret.data.author_id
+          });
+ 
+          console.log(users[userinfo])
+          console.log(ret.data)
+
+          let images = content.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g)
+          for (let i = 0; i < images.length; i++) {
+            content = content.replace(images[i], "<img style=\"max-height:300px;\" src=\"" + images[i] + "\">")
+          }
+
+          document.getElementById("title").innerHTML = ret.data.title
+         
+          document.getElementById("info").innerHTML = `Uploaded by <a style="color:` + users[userinfo].user_colour + `" href="profile.html?id=` + users[userinfo].id  + `"><b>` + users[userinfo].username + `</b></a> on <b>` + ret.data.timestamp + "</b><br>Device: <b>" + ret.data.device + "</b>"
+          document.getElementById("content").innerHTML = content.replace(/\n/g, "<br>")
+          if(ret.data.edited == true)
+          {
+            document.getElementById("content").innerHTML += "<br><br><label style=\"color:#828282\"><i>This paste has been edited.</i></label>"
+          }
+          document.title = "AbSq || " + ret.data.title + " by " + users[userinfo].username
+
+
+          
+          
+          
+      })
+      .catch(function(e){
+         document.write(`
+         <style>
+         body{
+
+            font-family: sans-serif;
+            text-align:center;
+         }
+         </style>
+         <br><br><h1>404</h1><br>Error: <b>` +  e + `</b>
+         <br><br>Are you trying to go to the <a href="home.html">home page</a>?`)
+      });
+
+
+  }
---
+++ profile.js
@@ -0,0 +1,82 @@
+var faunadb = window.faunadb
+var q = faunadb.query
+var client = new faunadb.Client({
+  secret: 'fnAEhgo6CoAAwWq8oF1VfjvulHEylVcOMB2UsUj9',
+  domain: 'db.eu.fauna.com',
+  scheme: 'https',
+})
+
+let urlParams = new URLSearchParams(window.location.search);
+
+function getProfileData(){
+
+    client.query(
+        q.Get(
+          q.Match(q.Index('user_by_id'), parseInt(urlParams.get("id")))
+        )
+      )
+            
+      .then(function(ret) {
+       
+        let verified = ""
+        let admin = ""
+        let dev = ""
+          if(ret.data.verified == true)
+          {
+              verified = `<i style="color:#1DA1F2; margin-left:5px; font-size:14px;" class="fas fa-badge-check"></i>`
+          }
+
+          if(ret.data.admin == true)
+          {
+              admin = `<i style="color:#d6220b; margin-left:5px; font-size:14px;" class="fas fa-user-shield"></i>`
+          }
+          if(ret.data.dev == true)
+          {
+              dev = `<i style="color:#575457; margin-left:5px; font-size:14px;" class="fas fa-cog"></i>`
+          }
+
+        document.getElementById("username").style.color = ret.data.colour
+    document.getElementById("username").innerHTML = "<b>" + ret.data.username + "</b>" + verified + admin + dev
+    document.getElementById("pfp").src = ret.data.profile_image
+    document.getElementById("uid").innerHTML = "UID: <b>" + ret.data.id + "</b>"
+    document.getElementById("bio").innerHTML = ret.data.bio.replace(/\n/g, "<br>")
+    document.title = "AbSq || " + ret.data.username
+
+    client
+    .query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection("pastes")), { size: 1000 }),
+        q.Lambda("X", q.Get(q.Var("X")))
+      )
+    )
+    .then(function (x) {
+       
+      x.data.reverse()
+      document.getElementById("pastecount").innerText = "Pastes [" + x.data.length + "]"
+      pastecount = x.data.length
+      for (let i = 0; i < x.data.length; i++) {
+
+          if(x.data[i].data.author_id == ret.data.id)
+          {
+              console.log(x.data[i])
+             document.getElementById("pastes").innerHTML += `<div class="paste">
+             <a href="https://submits.github.io?id=` + x.data[i].data.id + `" style="color: white; font-size: 20px;"><b>` +  x.data[i].data.title + `</b></a><br><br>
+             <p>Uploaded: <b>` +  x.data[i].data.timestamp + `</b><br>Device: <b>` +  x.data[i].data.device + `</b></p>
+                     </div>`
+          }
+
+      }
+
+    });
+
+
+
+      })
+      .catch(function(e){
+       document.write(`
+         Oopsie Daises!!!!!! 404!!!!!<br>Error: ` +  e + `
+         `)
+         console.error(e)
+      });
+
+}
