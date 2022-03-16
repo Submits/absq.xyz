cbbef42d735476c8294178b5337b2d384a506286
Submits <101422599+Submits@users.noreply.github.com>
2022-03-16 22:27:33 +0000

    Update profile.js

--- profile.js
+++ profile.js
@@ -173,23 +173,52 @@ let years = Math.round(diffTime / 1000 / 60 / 60 / 24 / 365)
 
         
        if(seconds >= 1 && seconds < 60){
-    return seconds + " seconds ago"
+        if(seconds == 1)
+        {
+          return seconds + " second ago"
+        }
+        else{
+          return second + " seconds ago"
+        }
 }
 else if(minutes >= 1 && minutes < 60){
+  if(minutes== 1)
+  {
+    return minutes + " minute ago"
+  }
+  else{
     return minutes + " minutes ago"
+  }
 }
 else if(hours >= 1 && hours < 24){
+  if(hours == 1)
+  {
+    return hours + " hour ago"
+  }
+  else{
     return hours + " hours ago"
+  }
 }
 else if(days >= 1 && days < 365){
+  if(days == 1)
+  {
+    return days + " day ago"
+  }
+  else{
     return days + " days ago"
+  }
 }
 else if(years >= 1){
+  if(years == 1)
+  {
+    return years + " year ago"
+  }
+  else{
     return years + " years ago"
+  }
 }
         
     }
-
 
 
      function follow(id){
